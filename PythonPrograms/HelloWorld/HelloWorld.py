8ed0f0c4563271d6527af75a9a5e83408fa77302
LuapRebel <paulleber23@gmail.com>
2017-02-12 07:24:08 -0800

    Initial commit

---
+++ PythonPrograms/HelloWorld/HelloWorld.py
@@ -0,0 +1,36 @@
+# print("Hello world")
+# print(1 + 2)
+# print(7 * 6)
+# print()
+# print("The End")
+# print("Python's strings are easy to use")
+# print('We can even use "quotes" in strings')
+# print("Hello" + " world")
+#
+# greeting = "Hello"
+# name = "Bruce"
+# print(greeting + name)
+# # We can add a space too
+# print(greeting + ' ' + name)
+
+# greeting = "Hello"
+# name = input("Please enter your name ")
+# print(greeting + ' ' + name)
+
+# splitString = "This string has been \nsplit over\nseveral\nlines"
+# print(splitString)
+#
+# tabbedString = "1\t2\t3\t4\t5"
+# print(tabbedString)
+
+print('The pet shop owner said "No, no, \'e\'s uh,...he\'s resting"')
+print("The pet shop owner said \"No, no, 'e's uh,...he's resting\"")
+
+anotherSplitString = """This string has been
+split over
+several lines"""
+
+print(anotherSplitString)
+
+print('''The pet shop owner said "No, no, 'e's uh,...he's resting"''')
+print("""The pet shop owner said "No, no, 'e's uh,...he's resting" """)
