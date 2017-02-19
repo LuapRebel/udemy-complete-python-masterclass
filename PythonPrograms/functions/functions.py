6b9dc4259c88a1cc854857fbf844d0e3a059c5ba
LuapRebel <paulleber23@gmail.com>
2017-02-19 14:16:56 -0800

    Add remainder of functions and challenge

--- PythonPrograms/functions/functions.py
+++ PythonPrograms/functions/functions.py
@@ -5,18 +5,23 @@ def python_food():
     print(" " * left_margin, text)
 
 
-def center_text(*args, sep=' ', end='\n', file=None, flush=False):
+def center_text(*args, sep=' '):
     text = ""
     for arg in args:
         text += str(arg) + sep
     left_margin = (80 - len(text)) // 2
-    print(" " * left_margin, text, end=end, file=file, flush=flush)
+    return " " * left_margin + text
 
 
-# python_food()
-center_text("spam and eggs")
-center_text("spam, spam, and eggs")
-center_text("spam, spam, spam, and eggs")
-center_text(123)
+# print(center_text("spam and eggs"))
+# print(center_text("spam, spam, and eggs"))
+# print(center_text("spam, spam, spam, and eggs"))
+# print(center_text(123))
+# print(center_text("first", "second", 3, 4, "spam", sep=":"))
 
-center_text("first", "second", 3, 4, "spam", sep=":")
+with open("menu", mode='w') as menu_file:
+    print(center_text("spam and eggs"), file=menu_file)
+    print(center_text("spam, spam, and eggs"), file=menu_file)
+    print(center_text("spam, spam, spam, and eggs"), file=menu_file)
+    print(center_text(123), file=menu_file)
+    print(center_text("first", "second", 3, 4, "spam", sep=":"), file=menu_file)
