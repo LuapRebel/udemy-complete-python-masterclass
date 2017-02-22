c677b21aebc36e14e193e535cd075e35a01aa2b9
LuapRebel <paulleber23@gmail.com>
2017-02-21 19:53:57 -0800

    Add lectures 133-134

--- PythonPrograms/Game/enemy.py
+++ PythonPrograms/Game/enemy.py
@@ -1,26 +1,29 @@
+import random
+
+
 class Enemy:
 
     def __init__(self, name="Enemy", lives=1, hit_points=0):
-        self.name = name
-        self.lives = lives
-        self.hit_points = hit_points
-        self.alive = True
+        self._name = name
+        self._lives = lives
+        self._hit_points = hit_points
+        self._alive = True
 
     def take_damage(self, damage):
-        remaining_points = self.hit_points - damage
+        remaining_points = self._hit_points - damage
         if remaining_points >= 0:
-            self.hit_points = remaining_points
-            print("I took {} points damage and have {} left".format(damage, self.hit_points))
+            self._hit_points = remaining_points
+            print("I took {} points damage and have {} left".format(damage, self._hit_points))
         else:
-            self.lives -= 1
-            if self.lives > 0:
-                print("{0.name} lost a life".format(self))
+            self._lives -= 1
+            if self._lives > 0:
+                print("{0._name} lost a life".format(self))
             else:
-                print("{0.name} is dead".format(self))
-                self.alive = False
+                print("{0._name} is dead".format(self))
+                self._alive = False
 
     def __str__(self):
-        return "Name: {0.name}, Lives: {0.lives}, Hit points: {0.hit_points}".format(self)
+        return "Name: {0._name}, Lives: {0._lives}, Hit points: {0._hit_points}".format(self)
 
 
 class Troll(Enemy):
@@ -30,10 +33,31 @@ class Troll(Enemy):
         super().__init__(name=name, lives=1, hit_points=23)
 
     def grunt(self):
-        print("Me {0.name}. {0.name} stomp you.".format(self))
+        print("Me {0._name}. {0._name} stomp you.".format(self))
 
 
 class Vampyre(Enemy):
 
     def __init__(self, name):
         super().__init__(name=name, lives=3, hit_points=12)
+
+    def dodges(self):
+        if random.randint(1, 3) == 3:
+            print("***** {0._name} dodges *****".format(self))
+            return True
+        else:
+            return False
+
+    def take_damage(self, damage):
+        if not self.dodges():
+            super().take_damage(damage=damage)
+
+
+class VampyreKing(Vampyre):
+
+    def __init__(self, name):
+        super().__init__(name=name)
+        self._hit_points = 140
+
+    def take_damage(self, damage):
+        super().take_damage(damage // 4)
--- PythonPrograms/Game/main.py
+++ PythonPrograms/Game/main.py
@@ -1,30 +1,38 @@
-from enemy import Enemy, Troll, Vampyre
+from enemy import Enemy, Troll, Vampyre, VampyreKing
 
-ugly_troll = Troll("Pug")
-print(ugly_troll)
-ugly_troll.take_damage(4)
-print(ugly_troll)
-
-another_troll = Troll("Ug")
-print(another_troll)
-
-brother = Troll("Urg")
-print(brother)
+# ugly_troll = Troll("Pug")
+# print(ugly_troll)
+# ugly_troll.take_damage(4)
+# print(ugly_troll)
+#
+# another_troll = Troll("Ug")
+# print(another_troll)
+#
+# brother = Troll("Urg")
+# print(brother)
 
 # ugly_troll.grunt()
 # another_troll.grunt()
 # brother.grunt()
 
-nosferatu = Vampyre("Nosferatu")
-print(nosferatu)
-nosferatu.take_damage(4)
-print(nosferatu)
+# nosferatu = Vampyre("Nosferatu")
+# # print(nosferatu)
+# nosferatu.take_damage(4)
+# # print(nosferatu)
+# #
+# # print("-" * 40)
+#
+# # another_troll.take_damage(30)
+# # print(another_troll)
+#
+# while nosferatu._alive:
+#     nosferatu.take_damage(1)
+#     # print(nosferatu)
+#
+# nosferatu._lives = 0
+# nosferatu._hit_points = 1
+# print(nosferatu)
 
-print("-" * 40)
-
-another_troll.take_damage(30)
-print(another_troll)
-
-while nosferatu.alive:
-    nosferatu.take_damage(1)
-    print(nosferatu)
+kingvlad = VampyreKing("King Vlad")
+print(kingvlad)
+kingvlad.take_damage(100)
