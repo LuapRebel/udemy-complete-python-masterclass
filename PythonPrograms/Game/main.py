5614c487bffd902f8611b2ee53e2930cbb66a232
LuapRebel <paulleber23@gmail.com>
2017-02-24 16:32:29 -0800

    Add lectures 135-137

--- PythonPrograms/Game/main.py
+++ PythonPrograms/Game/main.py
@@ -1,38 +1,7 @@
-from enemy import Enemy, Troll, Vampyre, VampyreKing
+a = 3
+b = "tim"
+c = 1, 2, 3
 
-# ugly_troll = Troll("Pug")
-# print(ugly_troll)
-# ugly_troll.take_damage(4)
-# print(ugly_troll)
-#
-# another_troll = Troll("Ug")
-# print(another_troll)
-#
-# brother = Troll("Urg")
-# print(brother)
-
-# ugly_troll.grunt()
-# another_troll.grunt()
-# brother.grunt()
-
-# nosferatu = Vampyre("Nosferatu")
-# # print(nosferatu)
-# nosferatu.take_damage(4)
-# # print(nosferatu)
-# #
-# # print("-" * 40)
-#
-# # another_troll.take_damage(30)
-# # print(another_troll)
-#
-# while nosferatu._alive:
-#     nosferatu.take_damage(1)
-#     # print(nosferatu)
-#
-# nosferatu._lives = 0
-# nosferatu._hit_points = 1
-# print(nosferatu)
-
-kingvlad = VampyreKing("King Vlad")
-print(kingvlad)
-kingvlad.take_damage(100)
+print(a)
+print(b)
+print(c)
