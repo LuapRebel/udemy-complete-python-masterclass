e51bc5d2a5bba2b9cc7d5d19674461a84021212b
LuapRebel <paulleber23@gmail.com>
2017-02-24 17:17:53 -0800

    Add lectures 138-140

--- PythonPrograms/Game/html_doc.py
+++ PythonPrograms/Game/html_doc.py
@@ -8,8 +8,8 @@ class Tag(object):
     def __str__(self):
         return "{0.start_tag}{0.contents}{0.end_tag}".format(self)
 
-    def display(self):
-        print(self)
+    def display(self, file=None):
+        print(self, file=file)
 
 
 class DocType(Tag):
@@ -23,6 +23,17 @@ class Head(Tag):
 
     def __init__(self):
         super().__init__('head', '')
+        self._head_contents = []
+
+    def add_tag(self, name, contents):
+        new_tag = Tag(name, contents)
+        self._head_contents.append(new_tag)
+
+    def display(self, file=None):
+        for tag in self._head_contents:
+            self.contents += str(tag)
+
+        super().display(file=file)
 
 
 class Body(Tag):
@@ -35,8 +46,50 @@ class Body(Tag):
         new_tag = Tag(name, contents)
         self._body_contents.append(new_tag)
 
-    def display(self):
+    def display(self, file=None):
         for tag in self._body_contents:
             self.contents += str(tag)
 
-        super().display()
+        super().display(file=file)
+
+
+class HtmlDoc(object):
+
+    def __init__(self):
+        self._doc_type = DocType()
+        self._head = Head()
+        self._body = Body()
+
+    def add_head_tag(self, name, contents):
+        self._head.add_tag(name, contents)
+
+    def add_body_tag(self, name, contents):
+        self._body.add_tag(name, contents)
+
+    def display(self, file=None):
+        self._doc_type.display(file=file)
+        print('<html>', file=file)
+        self._head.display(file=file)
+        self._body.display(file=file)
+        print('</html>', file=file)
+
+
+if __name__ == '__main__':
+    my_page = HtmlDoc()
+    my_page.add_head_tag('title', 'Demo HTML document')
+    my_page.add_body_tag('h1', 'Main heading')
+    my_page.add_body_tag('h2', 'subheading')
+    my_page.add_body_tag('p', 'This is a paragraph that will appear on the page')
+    with open('test.html', 'w') as test_doc:
+        my_page.display(file=test_doc)
+
+    new_body = Body()
+    new_body.add_tag('h1', 'Aggregation')
+    new_body.add_tag('p', "Unlike <strong>composition</strong>, aggregation uses existing instances"
+                          " of objects to build up another object.")
+    new_body.add_tag('p', "The composed object doesn't actually own the objects that it's composed of"
+                          " - if it's destroyed, those objects continue to exist.")
+
+    my_page._body = new_body
+    with open('test2.html', 'w') as test_doc:
+        my_page.display(file=test_doc)
